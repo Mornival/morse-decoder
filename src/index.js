25d7eaf29e4ac67571aefad94e29f18118ea92a7
Mornival <55550567+Mornival@users.noreply.github.com>
2020-10-15 20:23:54 +0300

    init: create project

--- src/index.js
+++ src/index.js
@@ -38,7 +38,48 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    for(let i1 = 0;expr.length % 10 !== 0;i1++){
+        expr += '0'
+    }
+    let str = "";
+    for(let i = 0;i < expr.length;i+=2){
+        if(expr[i] === '1'&&expr[i+1]==='0')
+        {
+            str +='.';
+        }
+        if(expr[i] === '1'&&expr[i+1]==='1'){
+            str += '-';
+        }
+        if(expr[i] === '*'&&expr[i+1]==='*'&&expr[i+2]==='*'&&expr[i+3]==='*'&&expr[i+4]==='*'&&expr[i+5]==='*'&&expr[i+6]==='*'&&expr[i+7]==='*'&&expr[i+8]==='*'&&expr[i+9]==='*')
+        {
+            str += ' ';
+        }
+        if((i + 2) % 10 === 0)
+        {
+            str += '1';
+        }
+    }
+    let unstr = "";
+    let newStr = "";
+    for(let j = 0; j < str.length; j++){
+        if(str[j] === ' ')
+        {
+            newStr += ' ';
+        }
+        if(str[j]!=='1')
+        {
+            unstr += str[j];
+        }
+        else
+        {
+            if(MORSE_TABLE.hasOwnProperty(unstr))
+            {
+                newStr += MORSE_TABLE[unstr];
+            }
+            unstr = "";
+        }
+    }
+    return newStr;
 }
 
 module.exports = {
